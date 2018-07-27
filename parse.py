d2eac5986ab84119526a2ac339ea61e98cde8cfa
Sigtran <sigtranie@gmail.com>
2018-07-27 12:57:03 +0100

    Update parse.py

--- parse.py
+++ parse.py
@@ -2,69 +2,46 @@ import sys, struct, codecs
 from itertools import islice
 
 members = [
-'Aggrriify',
-'akateksahn',
-'aversai',
-'Beef3687',
-'btgreat',
-'BuuDeV',
-'CanadianCnote',
-'candianhayden',
-'CCraider',
-'Chrismurf16',
-'Classydame',
-'DaDavemanCan',
-'davh9',
-'Demongamefreak',
-'Diederik69',
-'DrYoshiro',
-'EasyEgon',
-'EasyEgon777',
-'Featherstep',
-'GarrethXX',
-'gemzilou',
-'GGGkz',
-'Haidez',
-'hoskie',
-'iyyi',
-'jro012',
-'LazyMisted',
-'Lisk0',
-'logicbomb',
-'LordTwinkle',
-'mixhael',
-'MJMFOX',
-'MrPerfect988',
-'Neit007',
-'Noxv',
-'Pinchmiestro',
-'Piremard96',
-'quackpatton',
-'Sandmen',
-'Sarhezor',
-'Sicknessi',
-'Sigtrah',
-'Sigtran',
-'slicksean',
-'Tappoman',
-'thefrfcfgcc',
-'tospeli',
-'Tracebot21',
-'TrueDaBoMb',
-'truewarwizard',
-'VexApex',
-'Voobb',
-'whipplash',
-'xd4rkr4iderx',
-'yujamigit',
-'zoofo',
-'Zula1810'
+"Aggrriify",
+"akateksahn",
+"BuuDeV",
+"Chrismurf16",
+"DaDavemanCan",
+"davh9",
+"DrYoshiro",
+"EasyEgon",
+"Featherstep",
+"FrontalNgagemnT",
+"govnosrach",
+"greenxpg",
+"Haidez",
+"hegolas",
+"hegolas3",
+"hoskie",
+"kokkaburra",
+"logicbomb",
+"MJMFOX",
+"Monzarelli",
+"mrBira",
+"nebzdyashi",
+"Pinchmiestro",
+"Pinchster",
+"Sarhezor",
+"Sicknessi",
+"Sigtrah",
+"Sigtran",
+"slicksean",
+"storochbrun",
+"Tappoman",
+"tospeli",
+"truewarwizard",
+"VexApex"
     ]
 
 results = {}
 
 
-with open(sys.argv[1], "rb") as f:
+with open("gw.txt", "rb") as f:
     for line in f:
         for member in members:
             if '   '+member+'.' in line:
@@ -72,11 +49,15 @@ with open(sys.argv[1], "rb") as f:
                 #print member, data
                 might = (struct.unpack("<I", codecs.decode(data[4:12], "hex"))[0])/1000
                 score = struct.unpack("<h", codecs.decode(data[12:16], "hex"))[0]
-                results[member] = str(score) + '\t' + str(might)
+                if score > 0 or member not in results:
+			        results[member] = str(score) + '\t' + str(might) #+ '\r\n'
 
+#with open("results.txt", "wb") as f:
 for member in members:
-    try:
-        #print member +'\t', results[member]
-        print results[member]
-    except:
-        print member, 'NOT FOUND'
+	try:
+		#print member +'\t', results[member]
+		#f.write(results[member])
+		print results[member]
+	except:
+		#f.write(results[member])	
+		print member, 'NOT FOUND'
